54fc6536f3ccc0c0d79352d2bd9b7801ba0cb502
mohamedalhassan789 <mohammed-sk3@hotmail.com>
2025-10-19 18:02:37 +0400

    Delete secvuln-agent directory

--- secvuln-agent/src/notifiers/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Notifiers package for sending alerts via various channels
-"""
-
-from .notification_manager import NotificationManager
-
-__all__ = ['NotificationManager']
--- secvuln-agent/src/processors/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Processors package for CVE analysis and matching"""
-
-from .ai_analyzer import AIAnalyzer
-from .matcher import DeviceMatcher
-from .scorer import RiskScorer
-
-__all__ = ['AIAnalyzer', 'DeviceMatcher', 'RiskScorer']
